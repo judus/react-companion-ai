fb9c36e4f7d4c7f48bed5909e32d9740acafb0b1
Julien Duseyau <julien.duseyau@gmail.com>
2023-12-05 01:56:26 +0100

    Set a default image, quick fix

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,23 +1,20 @@
 export function imageFolder(folderName, imageUrl) {
+    const baseUrl = "https://storage.googleapis.com/companion-ai-bucket/images/";
     const defaultUrl = "https://storage.googleapis.com/companion-ai-bucket/images/q0hGs7GT9E2YMyaXpXJWOd8b.png";
 
-    // Check if folderName is not provided or if it's not in a valid format
-    if(!folderName || !folderName.trim() || imageUrl.indexOf("images") === -1) {
+    // Check if imageUrl is valid and contains 'images'
+    if(!imageUrl || imageUrl.indexOf("images") === -1) {
         return defaultUrl;
     }
 
-    // Ensure folderName ends with a "/"
-    if(!folderName.endsWith('/')) {
-        folderName += '/';
+    // Check if folderName is not provided or if it's not in a valid format
+    if(!folderName || !folderName.trim()) {
+        return imageUrl;
     }
 
-    // Split the URL by "/"
-    const parts = imageUrl.split('/');
+    // Ensure folderName does not start or end with a "/"
+    folderName = folderName.replace(/^\/+|\/+$/g, '');
 
-    // Find the index of "images" in the URL
-    const imageIndex = parts.findIndex(part => part === "images");
-
-    // Insert the folder name after "images" and rejoin the URL parts into a single string
-    parts.splice(imageIndex + 1, 0, folderName);
-    return parts.join('/');
+    // Replace the base URL with the base URL + folderName + "/"
+    return imageUrl.replace(baseUrl, `${baseUrl}${folderName}/`);
 }
